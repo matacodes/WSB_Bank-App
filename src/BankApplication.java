16f613e974635295316f544404654bb15b6cfb43
matacodes <ma.matysek@gmail.com>
2020-05-15 17:53:47 +0200

    General variables for Account: balance, lastTransation, customerID and customerName

--- src/BankApplication.java
+++ src/BankApplication.java
@@ -9,3 +9,13 @@ public class BankApplication {
 	}
 
 }
+
+// Class of bank account including four methods (deposit, withdraw, last transaction and menu) and one object (BankAccount)
+class BankAccount {
+    int balance; // Amount of money on customer's account
+    int lastTransaction; // Amount of money that can be withdrawn or deposited during last transaction
+    String customerID; // Customer ID
+    String customerName; // Customer Name
+
+
+}
