9a459b1fbdc89de69d966db965d1da777a19b848
matacodes <ma.matysek@gmail.com>
2020-05-15 18:15:27 +0200

    Show menu_method

--- src/BankApplication.java
+++ src/BankApplication.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class BankApplication {
 
@@ -5,6 +6,11 @@ public class BankApplication {
 		// TODO Auto-generated method stub
 		
 		System.out.println("Projekt ePortfel");
+		
+		//Creating new bank account with specific cust ID and name 
+		BankAccount account1 = new BankAccount("1234", "Kowalski");
+        account1.menu();
+
 
 	}
 
@@ -52,7 +58,64 @@ class BankAccount {
         }
     }
 
-    
+    // Method of showing menu
+    void menu() {
+        char option = '0';
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Klient: " + customerName);
+        System.out.println("ID Klienta: " + customerID);
+        System.out.println("");
+        System.out.println("1. Sprawdz balans konta");
+        System.out.println("2. Wplata");
+        System.out.println("3. Wyplata");
+        System.out.println("4. Ostatnia tranzakcja");
+        System.out.println("5. Koniec");
+
+        do {
+            System.out.println("** Wprowadz numer menu **");
+            option = scanner.next().charAt(0);
+            System.out.println();
+
+            switch (option) {
+
+            case '1':
+                System.out.println("Balans: " + balance);
+                System.out.println();
+                break;
+
+            case '2':
+                System.out.println("Wprowadz kwotê, któr¹ chcesz wp³aciæ:");
+                int amount = scanner.nextInt();
+                deposit(amount);
+                System.out.println();
+                break;
+
+            case '3':
+                System.out.println("Wprowadz kwotê, któr¹ chcesz wyp³aciæ:");
+                int amount1 = scanner.nextInt();
+                withdraw(amount1);
+                System.out.println();
+                break;
+
+            case '4':
+                getLastTransaction();
+                System.out.println();
+                break;
+
+            case '5':
+                System.out.println("Koniec.");
+                break;
+                
+            default:
+                System.out.println("Wprowadzono b³êdn¹ opcjê, spróbuj jeszcze raz.");
+                break;
+            }
+        } while (option != '5');
+        System.out.println("Dziêkujemy za korzystanie z naszego serwisu ePortfel. Do zobaczenia ponownie!");
+
+    }
+
 
 
     
